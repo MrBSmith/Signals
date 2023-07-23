b14a7d20bda3316dcc67f3c754f70c2d467ea09a
Hugo Baba <Hugobarette@gmail.com>
2023-07-23 17:40:39 +0200

    Character movement

--- Scenes/Character/Character.gd
+++ Scenes/Character/Character.gd
@@ -1,6 +1,8 @@
 extends Node2D
 class_name Character
 
+const MOVE_DURATION = 0.2
+
 var input_directions = {
 	"move_up": Vector2i.UP,
 	"move_down": Vector2i.DOWN,
@@ -10,9 +12,26 @@ var input_directions = {
 
 var cell = Vector2i.ZERO
 
+signal try_moving(dest_cell: Vector2i, callback: Callable)
+
 
 func _input(event: InputEvent) -> void:
 	var direction = _get_input_movement_direction(event)
+	
+	if direction != Vector2i.ZERO:
+		var dest_cell = cell + direction
+		try_moving.emit(dest_cell, move.bind(dest_cell))
+
+
+func move(dest_cell: Vector2i) -> void:
+	var offset := Vector2(Level.TILE_SIZE) / 2.0
+	var pos = Vector2(dest_cell * Level.TILE_SIZE) + offset
+	var tween = create_tween()
+	
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "position", pos, MOVE_DURATION)
+	
+	cell = dest_cell
 
 
 func _get_input_movement_direction(event: InputEvent) -> Vector2i:
--- Scenes/Level/Level.gd
+++ Scenes/Level/Level.gd
@@ -1,4 +1,25 @@
 extends Node2D
 class_name Level
 
+enum LAYERS {
+	GROUND,
+	WALLS
+}
+
 const TILE_SIZE = Vector2i(8, 8)
+
+@onready var tilemap: TileMap = %TileMap
+@onready var character: Node2D = %Character
+
+
+func _ready() -> void:
+	character.cell = Vector2i(character.position) / TILE_SIZE
+
+
+func is_cell_empty(cell: Vector2i) -> bool:
+	return not cell in tilemap.get_used_cells(LAYERS.WALLS)
+
+
+func _on_character_try_moving(dest_cell: Vector2i, move_callback: Callable) -> void:
+	if is_cell_empty(dest_cell):
+		move_callback.call()
